722bf1d7ccc9994f726ef7ea149fb35ce9fa926f
Mads Glahder <madsglahder@gmail.com>
2019-02-28 15:11:23 +0100

    altered CSV to be more object-y

--- src/main/java/application/CSVController.java
+++ src/main/java/application/CSVController.java
@@ -1,10 +1,56 @@
 package application;
 
+import com.sun.research.ws.wadl.HTTPMethods;
+import csv.CSV;
+import csv.CSVPerson;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CSVController {
 
+    @RequestMapping(value = "/original", method = RequestMethod.GET)
+    public ResponseEntity<Object> downloadOriginal(String param) throws IOException {
 
+        File file = new File("src/main/resources/testFile.csv");
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Expires", "0");
+        headers.add("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+
+    }
+
+    @RequestMapping(value = "/altered", method = RequestMethod.GET)
+    public ResponseEntity<Object> downloadAltered(String param) throws IOException {
+        CSV csv = new CSV();
+        csv.load("src/resources/testFile.csv");
+        csv.alterPeople();
+
+
+        //TODO
+
+    }
 
 }
--- src/main/java/csv/CSV.java
+++ src/main/java/csv/CSV.java
@@ -7,16 +7,48 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CSV {
-    public List<CSVPerson> load(String fileName) throws IOException {
-        Reader reader = Files.newBufferedReader(Paths.get("src/application.APIApplication/resources/" + fileName));
+
+    private List<CSVPerson> csvPeople;
+    private List<CSVPersonAltered> csvPersonAlteredList;
+
+    public void load(String fileName) throws IOException {
+        Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/" + fileName));
         CsvToBean csvBean = new CsvToBeanBuilder(reader)
                 .withType(CSVPerson.class)
                 .withIgnoreLeadingWhiteSpace(true)
                 .build();
 
-        return csvBean.parse();
+        this.csvPeople = csvBean.parse();
+    }
+
+    public void alterPeople() {
+        List<CSVPerson> csvPeople = this.csvPeople;
+
+        List<CSVPersonAltered> csvPersonAlteredList = new ArrayList<>();
+
+        for ( CSVPerson csvPerson : csvPeople) {
+
+            CSVPersonAltered alteredPerson = new CSVPersonAltered();
+            alteredPerson.setId(csvPerson.getId());
+            alteredPerson.setName(
+                    csvPerson.getFirstName() + " " + csvPerson.getLastName()
+            );
+            csvPersonAlteredList.add(alteredPerson);
+        }
+
+        this.csvPersonAlteredList = csvPersonAlteredList;
+    }
+
+    public List<CSVPerson> getCsvPeople() {
+        return csvPeople;
+    }
+
+    public List<CSVPersonAltered> getCsvPersonAlteredList() {
+        return csvPersonAlteredList;
     }
 }
+
